5dd7c2d9054a4da70a807b6116d723702120aceb
maximo fermin pereyra <pereyra.maximo@gmail.com>
2023-08-26 08:51:40 -0300

    cambios en producto

--- src/routes/product/Producto.jsx
+++ src/routes/product/Producto.jsx
@@ -1,18 +1,15 @@
 import "./Producto.css"
 import Productos from "../../components/Producto/Productos"
 import Tags from '../../components/Tag/Tags';
-import Menu from "../../components/Navbar/Navbar"
 
 const Product = () => {
 
     return (
 
     <div className='main-container'>  
-        <Tags>
-            
+        <Tags>            
         </Tags>   
-        <Productos />
-        
+        <Productos />        
     </div>
 
     )
