61281e3dab71a728001bbf7b213912dd55600662
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-07 19:46:01 -0300

    chore: seting base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pilis-mod6-web-x_app/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
