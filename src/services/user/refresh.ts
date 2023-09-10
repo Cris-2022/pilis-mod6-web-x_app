82a247943ce5a84f52fa596e0c0679e85d966db2
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-10 19:24:33 -0300

    refactor(services): update refresh user services
    implent Response Type interface

--- src/services/user/refresh.ts
+++ src/services/user/refresh.ts
@@ -1,4 +1,8 @@
-import { METHODS, createJsonBodyOptionsAuth } from '@/src/utils/request';
+import {
+  METHODS,
+  Response,
+  createJsonBodyOptionsAuth,
+} from '@/src/utils/request';
 
 import { PayloadAuth } from './types';
 import ENDPOINTS from '@/src/utils/endpoints';
@@ -9,20 +13,22 @@ interface Result {
   bearer_token: string;
 }
 
-async function refresh(refresh_token: string): Promise<Result> {
+async function refresh(refresh_token: string): Promise<Response<Result>> {
   const options = createJsonBodyOptionsAuth(refresh_token);
   const response = await fetch(ENDPOINTS.AUTH, {
     ...options,
     method: METHODS.PUT,
   });
 
-  if (!response.ok)
-    throw new Error('Operación fallida, vuelva a iniciar sesión');
+  const status = response.status;
+  const isError = response.ok;
+  if (isError) return { isError, status };
 
   const { token: bearer_token } = await response.json();
   const user: PayloadAuth = getPayload(bearer_token);
+  const body = { user, bearer_token };
 
-  return { user, bearer_token };
+  return { isError, status, body };
 }
 
 export default refresh;
