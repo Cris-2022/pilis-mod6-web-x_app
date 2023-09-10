51248a99ae240596d156b6b94f6a8c84aa5f3395
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-10 19:16:01 -0300

    feat(services): add updated user services

---
+++ src/services/user/updated.ts
@@ -0,0 +1,36 @@
+import {
+  METHODS,
+  Response,
+  createFormDataOptionAuth,
+} from '@/src/utils/request';
+import { UserData } from './types';
+import ENDPOINTS from '@/src/utils/endpoints';
+
+interface Body {
+  message: string;
+}
+
+async function updateUser(
+  bearer_token: string,
+  data: Partial<UserData>,
+): Promise<Response<Body>> {
+  const formData = new FormData();
+
+  if (data.avatar) formData.append('avatar', data.avatar);
+  if (data.username) formData.append('username', data.username);
+  if (data.password) formData.append('password', data.password);
+
+  const options = createFormDataOptionAuth(bearer_token, formData);
+  const response = await fetch(ENDPOINTS.USER, {
+    ...options,
+    method: METHODS.PUT,
+  });
+
+  const status = response.status;
+  if (response.ok) return { isError: false, status };
+
+  const body = await response.json();
+  return { isError: true, status, body };
+}
+
+export default updateUser;
