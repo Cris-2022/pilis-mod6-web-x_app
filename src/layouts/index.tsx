87935c1cb0aad28285863be3949b1c798558010e
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-11 00:52:43 -0300

    feat(layouts): implement useUserContext in layouts

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,8 +1,9 @@
+import { useUserContext } from '../context/user';
 import AdminLayouts from './AdminLayouts';
 import DefaultLayouts from './DefaultLayouts';
 
 export default function Layouts() {
-  const isLogin = false;
+  const { isLogin } = useUserContext();
 
   return isLogin ? <AdminLayouts /> : <DefaultLayouts />;
 }
