c48bd50ff369238577357bb11637e53f93ec7c9b
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-12 19:50:11 -0300

    feat(services): add addProduct service

---
+++ src/services/products/addProduct.ts
@@ -0,0 +1,34 @@
+import { METHODS, Response, createFormDataOptionAuth } from '@/utils/request';
+import { Product, ProductFormData } from './types';
+import ENDPOINTS from '@/utils/endpoints';
+
+type Result = Response<Product>;
+async function addProduct(
+  token: string,
+  data: Partial<ProductFormData>,
+): Promise<Result> {
+  const formData = new FormData();
+
+  if (data.category) formData.append('category', data.category);
+  if (data.name) formData.append('name', data.name);
+  if (data.price) formData.append('price', data.price.toString());
+  if (data.product) formData.append('product', data.product);
+  if (data.stock) formData.append('stock', data.stock.toString());
+
+  const options = createFormDataOptionAuth(token, formData);
+  const method = METHODS.POST;
+
+  const response = await fetch(ENDPOINTS.PRODUCTS, { ...options, method });
+
+  const status = response.status;
+  const isError = !response.ok;
+
+  if (isError) return { isError, status };
+
+  const result: Product = await response.json();
+  result.image = `${ENDPOINTS.PUBLIC}/${result.image}`;
+
+  return { isError, status, result };
+}
+
+export default addProduct;
