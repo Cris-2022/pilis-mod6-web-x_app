3619587da9a9ba8bebc1cf00f3c8c86aceaa9002
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-14 00:03:14 -0300

    feat(layouts): update default layouts

--- src/layouts/DefaultLayouts/Header.tsx
+++ src/layouts/DefaultLayouts/Header.tsx
@@ -3,17 +3,16 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <>
-      <header className='Navigation'>
-        <Link to='/'>
-          <img src='/src/assets/logo.png' className='logo' />
-        </Link>
-        <h1 className='Title'>xApp</h1>
-        <Link to='/Login' className='login'>
-          <img src='/src/assets/login.png' alt='' />
-          <h3 className='text-login'>Iniciar Sesión</h3>
-        </Link>
-      </header>
-    </>
+    <header className='Navigation'>
+      <Link to='/'>
+        <img src='/src/assets/logo.png' className='logo' />
+      </Link>
+      <h1 className='Title'>xApp</h1>
+
+      <Link to='/Login' className='login'>
+        <img src='/src/assets/login.png' alt='' />
+        <h3 className='text-login'>Iniciar Sesión</h3>
+      </Link>
+    </header>
   );
 }
