d95a93b6c1de555673ab9ab61f7405a194563bf6
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-15 14:31:05 -0300

    fix(pages): resolve product form

--- src/pages/Products/components/Form.tsx
+++ src/pages/Products/components/Form.tsx
@@ -1,6 +1,6 @@
-import { addProduct } from '@/services/products';
-import './Form.css'
-import { useForm } from 'react-hook-form';
+import { ProductFormData, addProduct } from '@/services/products';
+import './Form.css';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { useContext } from 'react';
 import { UserContext } from '@/context/user';
 import { ACTIONS } from '@/context/product/reducer/actions';
@@ -10,53 +10,51 @@ type FormValues = {
   name: string;
   category: string;
   price: number;
-  image: File;
-  stock: number
+  image: FileList;
+  stock: number;
 };
 
-const categoriesTest = [
-  "Bebidas",
-  "Hamburgesas",
-  "Sandwiches",
-  "Helados"
-];
-
+const categoriesTest = ['Bebidas', 'Hamburgesas', 'Sandwiches', 'Helados'];
 
 const Form = () => {
-  const { dispatch } = useContext(ProductContex)
-  const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({})
-  const { tokens } = useContext(UserContext)
+  const { dispatch } = useContext(ProductContex);
+  const { register, handleSubmit } = useForm<FormValues>({});
+  const { tokens } = useContext(UserContext);
 
-  const onSubmit = async (data: any) => {
-    console.log("DATA", data)
+  const onSubmit: SubmitHandler<FormValues> = async data => {
+    console.log('DATA', data);
 
-    dispatch({ type: ACTIONS.LOADING })
+    dispatch({ type: ACTIONS.LOADING });
     if (tokens) {
-      const token = tokens.bearer_token
-      const { isError, result } = await addProduct(token, data)
+      const product: ProductFormData = {
+        ...data,
+        image: data.image[0],
+      };
+
+      const token = tokens.bearer_token;
+      const { isError, result } = await addProduct(token, product);
       if (isError) {
-        alert(JSON.stringify(result))
-        return dispatch({ type: ACTIONS.ERROR })
+        alert(JSON.stringify(result));
+        return dispatch({ type: ACTIONS.ERROR });
       }
       dispatch({
         type: ACTIONS.ADDPRODUCT,
-        product: result
-      })
-      alert(JSON.stringify(result))
-    };
+        product: result,
+      });
+      alert(JSON.stringify(result));
+    }
   };
 
   return (
     <div className='head-prod'>
       <form onSubmit={handleSubmit(onSubmit)}>
-
         <input
-          type="text"
+          type='text'
           placeholder='Product name'
-          id="name"
-          {...register("name", { required: "The name es required" })}
+          id='name'
+          {...register('name', { required: 'The name es required' })}
         />
-        <select {...register("category")}>
+        <select {...register('category')}>
           {categoriesTest.map((category, index) => (
             <option key={index} value={category}>
               {category}
@@ -64,32 +62,25 @@ const Form = () => {
           ))}
         </select>
         <input
-          className="precio"
-          type="text"
+          className='precio'
+          type='text'
           placeholder='ingresar precio'
-          id="price"
-          {...register("price", { required: "The price es required" })}
+          id='price'
+          {...register('price', { required: 'The price es required' })}
         />
         <input
-          type="number"
+          type='number'
           placeholder='ingrese Stock'
-          id="stock"
-          {...register("stock", { required: "The stock es required" })}
+          id='stock'
+          {...register('stock', { required: 'The stock es required' })}
         />
-        <input
-          type="file"
-          id="imageInput"
-          {...register("image")}
-        />
-        <button
-          className='button-new'
-          type='submit'
-        >
+        <input type='file' id='imageInput' {...register('image')} />
+        <button className='button-new' type='submit'>
           Agregar
         </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
