982b9aa86a631d9c1d791304a801fe177c55a919
Luis Lavayen <lu14393@gmail.com>
2023-09-15 18:19:19 -0300

    product-se implemento eliminacion

--- src/pages/Products/components/Producto.tsx
+++ src/pages/Products/components/Producto.tsx
@@ -1,25 +1,32 @@
-
+import PropTypes from "prop-types"
 import './Producto.css'
 
 
+const Producto = ({ nombre, img, categoria, precio, productId, deleteProduct }) => {
 
-const Producto = ({ nombre, img, categoria, precio }) => {
-    
   return (
-<div className="cookieCard">
+    <div className="cookieCard">
       <h3 className="cookieHeading">{nombre}</h3>
       <h3 className="cookieDescription">imagen: {img}</h3>
       <h3 className="cookieDescription">categoria: {categoria}</h3>
       <h3 className="cookieHeading">precio: {precio} </h3>
       <div className="card-footer">
-         <button className='editButton'>Editar</button>
-         <button className='deleButton'>Eliminar</button>
-      </div>      
-      
-     
-</div>
+        <button className='editButton' >Editar</button>
+        <button className='deleButton' onClick={() => deleteProduct(productId)}>Eliminar</button>
+      </div>
+    </div>
 
-  )
-}
+  );
+};
+
+Producto.propsTypes = {
+  productId: PropTypes.string,
+  nombre: PropTypes.string,
+  categoria: PropTypes.string,
+  stock: PropTypes.number,
+  img: PropTypes.string,
+  precio: PropTypes.number,
+  deleteProduct: PropTypes.func
+};
 
 export default Producto;
