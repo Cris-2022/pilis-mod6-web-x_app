6da6d195343004fbef3ee73750ab21262b589151
red-mat <jesus.ing.rodriguez@gmail.com>
2023-09-16 08:31:34 -0300

    resolve main banner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,18 @@
 import { Tarjetas } from '../../layouts/components/Tarjetas';
 import './style.css';
+import banner_1 from '@/assets/Carousel/1.jpg';
+import banner_2 from '@/assets/Carousel/2.jpg';
+import banner_3 from '@/assets/Carousel/3.jpg';
+import banner_4 from '@/assets/Carousel/4.jpg';
 
 export default function Home() {
   return (
     <div>
       <section className='automatic'>
-        <img className='slider-imag' src='/src/assets/Carousel/1.jpg' alt='' />
-        <img className='slider-imag' src='/src/assets/Carousel/2.jpg' alt='' />
-        <img className='slider-imag' src='/src/assets/Carousel/3.jpg' alt='' />
-        <img className='slider-imag' src='/src/assets/Carousel/4.jpg' alt='' />
+        <img className='slider-imag' src={banner_1} alt='' />
+        <img className='slider-imag' src={banner_2} alt='' />
+        <img className='slider-imag' src={banner_3} alt='' />
+        <img className='slider-imag' src={banner_4} alt='' />
       </section>
       <div className='grid-card'>
         <Tarjetas />
