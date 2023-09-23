bd41405d1e26d1b6ece2048d07220a6bb392072c
Luis Lavayen <lu14393@gmail.com>
2023-09-23 04:30:51 -0300

    arreglos menores

--- src/pages/Orders/components/Order.tsx
+++ src/pages/Orders/components/Order.tsx
@@ -113,11 +113,3 @@ const Order = ({ id, Fecha_hora, estado, total, code, description }: Props) => {
 };
 
 export default Order;
-{/* <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div> */}
